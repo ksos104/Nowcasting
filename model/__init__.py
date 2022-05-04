5882362ee7730afe5c00c947fcbaa1562925d043
msson <ksos0104@gamil.com>
2022-05-04 11:29:40 +0900

    UNet implementation

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,4 @@
 from .criterion import GDL, temporal_weight_func, MSELoss, BiPatchNCE, L1Loss, GANLoss
 from .VPTR_modules import VPTREnc, VPTRDec, VPTRDisc, VPTRFormerNAR, VPTRFormerFAR
 from .ResNetAutoEncoder import init_weights
+from .unet import unet
