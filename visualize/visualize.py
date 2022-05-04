932b82e12bb766fa8b02425b5464b9de96a701cd
jinyxp <verdad4824@kaist.ac.kr>
2022-05-04 21:20:16 +0900

    minor update
    minor update

--- visualize/visualize.py
+++ visualize/visualize.py
@@ -7,22 +7,25 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt 
 import scipy.ndimage as ndimage
 
-date = '201907260300'
-vid = np.load('./save/' + date + '.npy')
-vid = ndimage.rotate(vid, 180, reshape=True)
+def visualize_clip(path,file_name):
+    vid = np.load(path)
+    vid = ndimage.rotate(vid, 180, reshape=True)
+    fig = plt.figure()
+    camera = Camera(fig)
+    ax = fig.add_subplot(projection = ccrs.PlateCarree())
 
-fig = plt.figure()
-camera = Camera(fig)
-ax = fig.add_subplot(projection = ccrs.PlateCarree())
+    for img in vid :
+        ax.set_extent([122,131.1,43,33]) 
+        ax.coastlines('50m')
+        im = ax.imshow(img, extent=(122,131.1,43,33))
+        ax.plot()
+        ax.plot(extent=(122,131.1,43,33),interpolation='nearest',transform = ccrs.PlateCarree())
+        camera.snap()
 
-for img in vid :
-    ax.set_extent([122,131.1,43,33]) 
-    ax.coastlines('50m')
-    im = ax.imshow(img, extent=(122,131.1,43,33))
-    ax.plot()
-    ax.plot(extent=(122,131.1,43,33),interpolation='nearest',transform = ccrs.PlateCarree())
-    camera.snap()
-
-
-animation = camera.animate(interval=100, blit=True)
-animation.save('sine.gif')
+    animation = camera.animate(interval=100, blit=True)
+    animation.save(file_name)
+    
+if __name__ == '__main__':
+    dataset = 'KTPW' #see utils.dataset
+    root = './data/kTPW/train/201907260300.npy'
+    visualize_clip(root, './full_clip.gif')
